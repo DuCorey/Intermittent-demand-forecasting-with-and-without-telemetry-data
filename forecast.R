133d220b612e6ef151b4260acb65200db72d2292
Corey Ducharme <corey.ducharme@polymtl.ca>
2019-11-13 13:14:44 -0500

    Move ets smoothing from forecast to smoothing

--- forecast.R
+++ forecast.R
@@ -10,19 +10,3 @@ library(forecast)
 source("series.R")
 
 #' functions
-my_ets <- function(data, ...)
-{
-    #' Wrapper function over forecast::ets
-    if (nrow(data) < 1) {
-        warning("ets requires a minimum of 1 value in the time series. Returning NULL.")
-        return(NULL)
-    }
-
-    ## Convert data to numeric since the method don't like to take xts input
-    res <- fitted(forecast::ets(as.numeric(data), ...)) %>%
-        xts(., order.by = index(data))
-
-    attr(res, 'frequency') <- frequency(data)
-
-    return(res)
-}
--- smooth.R
+++ smooth.R
@@ -3,6 +3,7 @@
 #' author: Corey Ducharme / corey.ducharme@polymtl.ca
 
 #' packages
+library(forecast)
 
 #' imports
 
@@ -13,6 +14,24 @@ ksmooth_xts <- function(data, ...)
     res <- ksmooth(index(data), data, ...)$y
 
     res <- xts(res, order.by = index(data))
+    attr(res, 'frequency') <- frequency(data)
+
+    return(res)
+}
+
+ets_smooth_xts <- function(data, ...)
+{
+    #' automatic Exponential Smoothing
+    if (nrow(data) < 1) {
+        warning("ets requires a minimum of 1 value in the time series. Returning NULL.")
+        return(NULL)
+    }
+
+    ## Convert data to numeric since the method don't like to take xts input
+    smooth <- fitted(forecast::ets(as.numeric(data), ...))
+
+        xts(., order.by = index(data))
+
     attr(res, 'frequency') <- frequency(data)
 
     return(res)
