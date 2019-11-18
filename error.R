c9193de3882cdd45957092e1911923028fa31b49
Corey Ducharme <corey.ducharme@polymtl.ca>
2019-11-18 12:37:49 -0500

    Implement scaled and PIS errors

--- error.R
+++ error.R
@@ -8,25 +8,60 @@ library(smooth)
 #' imports
 
 #' functions
-my_mase <- function(x, f)
+my_MASE <- function(x, f)
 {
     Q <- sum(abs(diff(x)), na.rm = TRUE) / (length(x) - 1)
-    res <- smooth::MASE(x, f, Q)
+    res <- greybox::MASE(x, f, Q)
     return(res)
 }
 
 
-error_calc <- function(x, f, type)
+my_sME <- function(x, f, scale)
 {
-    type <- match.arg(type, c("mase", "mape", "rmse", "mae"))
+    #' scaled Mean Error
+    res <- mean(x - f, na.rm = TRUE)/scale
+    return(res)
+}
+
+
+my_sMAE <- function(x, f, scale)
+{
+    #' scaled Mean Absolute Error
+    return(greybox::MAE(x, f)/scale)
+}
+
+
+my_PIS <- function(x, f)
+{
+    #' Period In Stock
+    return(sum(cumsum(f - x)))
+}
+
+
+my_sAPIS <- function(x, f, scale)
+{
+    #' scaled Absolute Period In Stock
+    return(abs(my_PIS(x,f))/scale)
+}
+
+
+error_calc <- function(x, f, type, scale = NULL)
+{
+    type <- match.arg(type, c("MAE", "RMSE", "MASE", "sME", "sMAE", "sMSE", "PIS", "sPIS", "sAPIS"))
 
     x %<>% as.numeric
     f %<>% as.numeric
     err <- switch(type,
-                  mase = my_mase(x, f),
-                  mape = smooth::MAPE(x, f),
-                  rmse = sqrt(smooth::MSE(x, f)),
-                  mae = smooth::MAE(x, f))
+                  MAE = greybox::MAE(x, f),
+                  RMSE = sqrt(greybox::MSE(x, f)),
+                  MASE = my_MASE(x, f),
+                  sME = my_sME(x, f, scale),
+                  sMAE = my_sMAE(x, f, scale),
+                  sMSE = greybox::sMSE(x, f, scale),
+                  PIS = my_PIS,
+                  sPIS = greybox::sPIS(x, f, scale),
+                  sAPIS = my_sAPIS)
+
     return(err)
 }
 
