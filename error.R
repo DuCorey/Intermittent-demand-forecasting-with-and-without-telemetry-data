477660fc953c0452fc276ce488bb7e3d1951c3d9
Corey Ducharme <corey.ducharme@polymtl.ca>
2020-05-08 16:49:02 -0400

    Removed next day under safety level

--- error.R
+++ error.R
@@ -64,37 +64,3 @@ error_calc <- function(x, f, type, scale = NULL)
 
     return(err)
 }
-
-
-next_day_under_safety_level <- function(del, con, peak, safety_level) {
-    amount <- 0
-    con_start <- which(index(con) == date(del))
-    a <- 0
-    safety_stock <- peak - safety_level
-
-    while(amount <= peak) {
-        amount <- amount + con[[con_start + a]]
-        a <- a + 1
-    }
-
-    ## We return the date difference
-    return(as.numeric(index(con[con_start + a - 1]) - del))
-}
-
-
-try_next_day_under_safety_level <- function(del, con, peak, safety_level) {
-    res <- tryCatch({
-        next_day_under_safety_level(del, con, peak, safety_level)
-    }, error = function(cond) {
-        return(NA)
-    })
-    return(res)
-}
-
-
-ndusl_metric <- function(table)
-{
-    #' Return a single value to evaluate the accuracy of the next day under
-    #' safety level performance metric
-    return(as.numeric(abs(as.numeric(names(table))) %*% as.numeric(table)))
-}
