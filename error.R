1f4af3b1d18cdf42dd14eee984433957b1d57539
Corey Ducharme <corey.ducharme@polymtl.ca>
2020-05-30 16:58:16 -0400

    Add sPIS and sMSE

--- error.R
+++ error.R
@@ -8,45 +8,53 @@ library(smooth)
 #' imports
 
 #' functions
-my_MASE <- function(x, f)
-{
+my_MASE <- function(x, f) {
+    #' Mean Absolute Scaled Error
     Q <- sum(abs(diff(x)), na.rm = TRUE) / (length(x) - 1)
     res <- greybox::MASE(x, f, Q)
     return(res)
 }
 
 
-my_sME <- function(x, f, scale)
-{
+my_sME <- function(x, f, scale) {
     #' scaled Mean Error
     res <- mean(x - f, na.rm = TRUE)/scale
     return(res)
 }
 
 
-my_sMAE <- function(x, f, scale)
-{
+my_sMAE <- function(x, f, scale) {
     #' scaled Mean Absolute Error
     return(greybox::MAE(x, f)/scale)
 }
 
 
-my_PIS <- function(x, f)
-{
+my_sMSE <- function(x, f, scale) {
+    #' scaled Mean Squared Error
+    res <- mean(((x - f)/scale)**2)
+    return(res)
+}
+
+
+my_PIS <- function(x, f) {
     #' Period In Stock
     return(sum(cumsum(f - x)))
 }
 
 
-my_sAPIS <- function(x, f, scale)
-{
+my_sPIS <- function(x, f, scale) {
+    #' scaled Period In Stock
+    return(my_PIS(x,f)/scale)
+}
+
+
+my_sAPIS <- function(x, f, scale) {
     #' scaled Absolute Period In Stock
     return(abs(my_PIS(x,f))/scale)
 }
 
 
-error_calc <- function(x, f, type, scale = NULL)
-{
+error_calc <- function(x, f, type, scale = NULL) {
     type <- match.arg(type, c("MAE", "RMSE", "MASE", "sME", "sMAE", "sMSE", "PIS", "sPIS", "sAPIS"))
 
     x %<>% as.numeric
